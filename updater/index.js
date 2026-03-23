f44b1955571293bab62b83f0fb5cefffa62caed7
Jan Henk Edens <jh.edens@hotmail.nl>
2026-03-23 12:37:17 +0100

    fix: final resolver for updater errors by bypassing the socket proxy

--- updater/index.js
+++ updater/index.js
@@ -92,7 +92,6 @@ app.post('/update', (req, res) => {
       const { stdout, stderr } = await execPromise(cmd, { 
         env: { 
           ...process.env, 
-          DOCKER_HOST: 'tcp://docker-socket-proxy:2375',
           COMPOSE_PROJECT_NAME: 'aetheros', // explicitly set project name to avoid mismatches
           DOCKER_BUILDKIT: '0',
           COMPOSE_DOCKER_CLI_BUILD: '0'
